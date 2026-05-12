7b0c98a2cbfbdbbb4390768c2014d11788194c1c
PedroUmp <pedro_u@id.uff.br>
2026-05-12 20:40:50 -0300

    Add files via upload

--- main.hs
+++ main.hs
@@ -2,15 +2,27 @@ import System.Environment (getArgs)
 import Data.List (sortBy)
 import qualified Data.Map as M
 
+replaceChar :: String -> Char -> Char
+replaceChar sep c = if c `elem` sep then ' ' else c
+
 replaceSeparators :: String -> String -> String
-replaceSeparators sep text = map (\c -> if c `elem` sep then ' ' else c) text
+replaceSeparators sep text = map (replaceChar sep) text
+
+toWeight :: [String] -> String -> (String, Int)
+toWeight res w = (w, if w `elem` res then 2 else 1)
 
 getFrequencies :: [String] -> String -> String -> M.Map String Int
 getFrequencies res sep text =
     let cleanText = replaceSeparators sep text
-        tokens = words cleanText
-        weights = map (\w -> (w, if w `elem` res then 2 else 1)) tokens
+        tokens = words cleanText //'arroz com feijao' -> ['arroz', 'com', feijao']
+        weights = map (toWeight res) tokens
     in M.fromListWith (+) weights
+
+compareEntries :: (String, Int) -> (String, Int) -> Ordering
+compareEntries (w1, f1) (w2, f2) = compare f2 f1 <> compare w1 w2
+
+printEntry :: (String, Int) -> IO ()
+printEntry (w, f) = putStrLn $ w ++ ": " ++ show f
 
 main :: IO ()
 main = do
@@ -21,28 +33,28 @@ main = do
             let [fRes, fSep, fC1, fC2] = args
             resContent <- readFile fRes
             sepContent <- readFile fSep
-            c1Content <- readFile fC1
-            c2Content <- readFile fC2
+            c1Content  <- readFile fC1
+            c2Content  <- readFile fC2
 
-            let res = words resContent 
+            let res      = words resContent
             let sepChars = filter (`notElem` "\n\r") sepContent
 
             let freq1 = getFrequencies res sepChars c1Content
                 freq2 = getFrequencies res sepChars c2Content
 
-            let sortedF1 = sortBy (\(w1, f1) (w2, f2) -> compare f2 f1 <> compare w1 w2) (M.toList freq1)
+            let sortedF1 = sortBy compareEntries (M.toList freq1)
 
             let isSimilar f1 f2 = 10 * abs (f1 - f2) <= max f1 f2
                 m = sum [ f1 | (w, f1) <- M.toList freq1
                              , let f2 = M.findWithDefault 0 w freq2
                              , isSimilar f1 f2 ]
-                
-                somaF1 = sum (M.elems freq1)
+
+                somaF1      = sum (M.elems freq1)
                 similaridade = if somaF1 == 0 then 0.0 else fromIntegral m / fromIntegral somaF1
 
-            putStrLn "=== RELATÓRIO DE FREQUÊNCIAS (C1) ==="
-            mapM_ (\(w, f) -> putStrLn $ w ++ ": " ++ show f) sortedF1
-            
+            putStrLn "Relatório de Frequências(C1)"
+            mapM_ printEntry sortedF1
+
             putStrLn "\n=== MÉTRICAS DE SIMILARIDADE ==="
             putStrLn $ "Valor de m    : " ++ show m
             putStrLn $ "Soma(f1)      : " ++ show somaF1
